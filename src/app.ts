1861c9965ead9f360b644ad59fd53bcacc643770
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2022-05-30 10:08:35 +0300

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, {Express, Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import pino from 'pino';
+
+dotenv.config();
+const logger = pino();
+// logger.info('Heya Pino');
+const PORT = process.env.PORT || 3000;
+
+const app: Express = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('common'));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('<p>Welcome to Greatness</p>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT} ⚡`);
+});
---
+++ tests/app.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+describe('App Test', () => {
+
+    it('should pass', () => {
+        var msg = 'test';
+        expect(msg).to.be.ok;
+    });
+
+});
